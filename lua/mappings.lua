b2bfb44ed15fe917cc920b7fb665ae588304fc64
rohith <=>
2025-02-22 11:47:07 +0530

    save

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,14 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("i", "jj", "<ESC>")
+
+
+-- keymap for angular
+map("n","<leader>ac",":e %:r.ts<CR>", {noremap = true, silent = true })
+map("n","<leader>ah",":e %:r.html<CR>", {noremap = true, silent = true })
+map("n","<leader>as",":e %:r.scss<CR>", {noremap = true, silent = true })
+map("n","<leader>at",":e %:r.spec.ts<CR>", {noremap = true, silent = true })
+
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
