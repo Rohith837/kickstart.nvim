730d2dd9e1e31e4246fd170bc1442dbc9635be8d
rohith <kamurohith748@gmail.com>
2025-03-01 09:40:31 +0530

    space to tab

--- init.lua
+++ init.lua
@@ -162,6 +162,7 @@ vim.opt.scrolloff = 20
 
 vim.opt.tabstop = 4 -- Number of spaces that a <Tab> counts for
 vim.opt.shiftwidth = 4
+vim.opt.expandtab = false  -- Use tabs instead of spaces
 
 vim.opt.fileformat = 'unix'
 
