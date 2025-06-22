c1f8433ad67d9c74951e2707ba614303bb4bd16e
Rohith Kamu <rohit.kamu@trinitymobility.com>
2025-06-22 17:47:56 +0530

    path display changed for telescope

--- init.lua
+++ init.lua
@@ -476,6 +476,40 @@ else
 
         -- [[ Configure Telescope ]]
         -- See `:help telescope` and `:help telescope.setup()`
+        local function filename_first_path_display(_, path)
+          local tail = vim.fs.basename(path)
+          local parent = vim.fs.dirname(path)
+          if parent == '.' then
+            return tail
+          else
+            return string.format('%s\t\t%s', tail, parent)
+          end
+        end
+
+        local function path_display_last_n_dirs(n)
+          return function(opts, path)
+            local filename = vim.fs.basename(path)
+            local parts = vim.split(path, '[/\\]') -- Cross-platform path splitting
+
+            -- Remove the filename from parts (it's the last element)
+            table.remove(parts)
+
+            if #parts >= n then
+              -- Take last n directories
+              local dirs = {}
+              for i = #parts - n + 1, #parts do
+                table.insert(dirs, parts[i])
+              end
+              return table.concat(dirs, '/') .. '/' .. filename
+            elseif #parts > 0 then
+              -- Show all available directories if less than n
+              return table.concat(parts, '/') .. '/' .. filename
+            else
+              -- Just filename if no directories
+              return filename
+            end
+          end
+        end
         require('telescope').setup {
           -- You can put your default mappings / updates / etc. in here
           --  All the info you're looking for is in `:help telescope.setup()`
@@ -498,6 +532,11 @@ else
             mappings = {
               i = { ['<c-enter>'] = 'to_fuzzy_refine' },
             },
+            -- path_display = filename_first_path_display,
+            path_display = path_display_last_n_dirs(1),
+            -- path_display = { 'smart' },
+            -- path_display = { "truncate" }
+            -- path_display = { 'tail' },
           },
           extensions = {
             ['ui-select'] = {
@@ -526,7 +565,10 @@ else
         vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
         vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
         vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-        vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+        -- vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+        vim.keymap.set('n', '<leader>sg', function()
+          require('custom/utils/telescope-multi-grep').setup()
+        end, { desc = '[S]earch by [G]rep' })
         vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
         vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
         vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
@@ -554,6 +596,14 @@ else
         vim.keymap.set('n', '<leader>sn', function()
           builtin.find_files { cwd = vim.fn.stdpath 'config' }
         end, { desc = '[S]earch [N]eovim files' })
+
+        vim.keymap.set('n', '<leader>spf', function()
+          builtin.find_files { cwd = vim.fs.joinpath(vim.fn.stdpath 'data', 'lazy') }
+        end, { desc = '[S]earch [P]lugins [F]ind source files' })
+
+        vim.keymap.set('n', '<leader>spg', function()
+          require('custom/utils/telescope-multi-grep').setup { cwd = vim.fs.joinpath(vim.fn.stdpath 'data', 'lazy') }
+        end, { desc = '[S]earch [P]lugins [G]rep' })
       end,
     },
 
