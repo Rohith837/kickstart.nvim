e20d92c3b5b8ffb91b5de95c61b44b8e2b83b6f4
Rohith Kamu <rohit.kamu@trinitymobility.com>
2025-06-24 21:17:04 +0530

    Buffer manager

---
+++ lua/custom/plugins/buffer-manager.lua
@@ -0,0 +1,17 @@
+return {
+  'j-morano/buffer_manager.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  config = function()
+    require('buffer_manager').setup {
+      short_file_names = true,
+    }
+
+    local buffer_manager_ui = require 'buffer_manager.ui'
+
+    vim.keymap.set('n', '<leader>bn', buffer_manager_ui.nav_next, { desc = 'Go To [N]ext Buffer' })
+    vim.keymap.set('n', '<leader>bp', buffer_manager_ui.nav_prev, { desc = 'Go To [P]revious Buffer' })
+    vim.keymap.set('n', '<leader>bm', buffer_manager_ui.toggle_quick_menu, { desc = 'Toggle [B]uffer [M]enu' })
+  end,
+}
